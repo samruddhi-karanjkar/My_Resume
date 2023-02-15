c09ccc062e053f8082c743ef0292db8088516c43
Sourabh Hodage <sourabhh3838@gmail.com>
2023-02-15 10:18:25 +0530

    initial commit

---
+++ src/config/Config.tsx
@@ -0,0 +1,192 @@
+// A
+export const NAME = "Sourabh Hodage";
+export const JOB_TITLE = "Aspiring Data Analyst";
+export const LOCATION = "Gadhinglaj, Maharashtra";
+
+// B
+export const CONTACT = [
+    {
+        "name": "Github",
+        "link": "https://github.com/Souru22",
+        "value": "github/Souru22",
+        "icon": "",
+        "margin": "32px"
+    },
+    {
+        "name": "Linkedin",
+        "link": "https://www.linkedin.com/in/sourabh-hodage/",
+        "value": "linkedin/sourabh-hodage",
+        "icon": "",
+        "margin": "22px"
+    },
+    {
+        "name": "Email",
+        "link": "mailto:sshodage3292@gmail.com",
+        "value": "sshodage3292@gmail.com",
+        "icon": "",
+        "margin": "40px"
+    },
+    {
+        "name": "Phone",
+        "link": "tel:+919260093292",
+        "value": "+919260093292",
+        "icon": "",
+        "margin": "34px"
+    }
+]
+
+// C
+export const SUMMARY = "Recent graduate with a strong foundation in data analysis and machine learning from Imarticus Learning. Proficient in SQL, Python (Pandas, NumPy, Seaborn, Matplotlib) and Power BI with a passion for solving real-world problems through data-driven approaches. Seeking a data analyst role where I can apply my skills and continue learning in a dynamic and collaborative environment."
+// D
+export const EXPERIENCE = [
+    {
+        "title": "Internship",
+        "date": "Jan 2022 - June 2022",
+        "company": "Technorrithm Engineering Pvt.Ltd",
+        "bullets": ["Designing of Bulk Milk Cooler",
+            "Analysis Field in Products",
+            "Business related visualizations",
+            "Finding Thermodynamic Conditions applied on it.",
+            "Preparing a standard Design which can affordable for customers as well as Small Scale Industries"
+        ]
+    },
+
+]
+
+// E
+export const EDUCATION = [
+    {
+        "title": "Post Graduate Program in Data Analytics",
+        "date": "December 2022",
+        "company": "Imarticus Learning,Pune",
+        "details": [
+            {
+                "title": "Specilization",
+                "description": "MS SQL,Python,Machine Learning,Deep Learning,Power BI, Tableau "
+            }
+        ]
+    },
+    {
+        "title": "BTech in Mechanical Engineering",
+        "date": "2022",
+        "company": "Rajarambapu Institute of Technology, Islampur",
+        "details": [
+            {
+                "title": "Grade",
+                "description": "First Class, 6.75 CGPI"
+            },
+        ]
+    },
+
+
+]
+
+
+
+// F
+export const PROJECTS = [
+    {
+        "title": "Linear Regression Model of Fish Data",
+        "url": "https://github.com/Souru22/-Linear-Regression-model-Fish-Weight/tree/main",
+        "github": "https://github.com/Souru22/-Linear-Regression-model-Fish-Weight/tree/main",
+        "date": "June 2022",
+        "technologies": [
+            "Python",
+            "Machine Learning",
+            "visualization",
+            "Pandas",
+            "Numpy",
+            "Seaborn",
+            "Sklearn"
+
+        ],
+        "specs": [
+            "By using Linear Regression mode",
+            "Study of Fish Data for prediction of their weight",
+            "In this project I have worked on Fish Data for predicting that what will be the weight of Fish by various variables",
+            "Weight of Fish is Numerical, hence I applied Linear Regression",
+            "I worked on this data to find out what are factors affecting the weight of Fish."
+        ]
+    },
+    {
+        "title": "Decision Tree Model of Churn Customers",
+        "url": "https://github.com/Souru22/Random-Forest-model-of-Churn-Customers",
+        "github": "https://github.com/Souru22/Random-Forest-model-of-Churn-Customers",
+        "date": "August 2022",
+        "technologies": [
+            "Python",
+            "Machine Learning",
+            "visualization",
+            "Pandas",
+            "Numpy",
+            "Seaborn",
+            "Sklearn"
+
+        ],
+        "specs": [
+            "By using Random Forest (Classification) Model",
+            "Study of Churn Data for prediction of customer",
+            "In this project I have worked on Churn Data for predicting that customers will churn or not",
+            "Churn status is classified as Yes or No",
+            "I worked on this data to find out what are factors affecting the status for the customers to churn the company"
+        ]
+    },
+    {
+        "title": "Music Recommendation System on Spotify",
+        "url": "https://github.com/Souru22/by-using-K-Means-Music-Recommendation-System-on-Spotif-",
+        "github": "https://github.com/Souru22/by-using-K-Means-Music-Recommendation-System-on-Spotif-",
+        "date": "November 2022",
+        "technologies": [
+            "Python",
+            "Machine Learning",
+            "visualization",
+            "Pandas",
+            "Numpy",
+            "Seaborn",
+            "Sklearn",
+            "K-Means"
+
+        ],
+        "specs": [
+            "By using K-Means Clustering Model and Euclidian distance method",
+            "Spotify Recommendation Engine for creating playlists based on user behavior",
+            "Non-Negative Matrix Factorization (NFM) For Decomposing Data",
+            "For for predicating the top 5 users and the top 5 songs heard and evaluating it using K-Means clustering"
+        ]
+
+    }
+]
+
+// G
+export const SKILLS = [
+    {
+        "title": "Languages",
+        "skills": [
+            "Python", "SQL", "C Language"
+        ]
+    },
+    {
+        "title": "Data Analysis ",
+        "skills": [
+            "SQL (MS SQL Server) ",
+            "Python",
+            "Machine Learning and Deep Learning",
+            "Statistics Fundamentals",
+            "MS Office Tools",
+            "Tableau",
+            "Power BI"
+        ]
+    },
+    {
+        "title": "Tools & Packages",
+        "skills": [
+            "Linear Regression", "Logistic Regression",
+            "Decision Tree",
+            "Random Forest",
+            "SVM", "PCA", "K-Means Clustering",
+            "Pandas", "Numpy", "Scikit-Learn",
+            "Seaborn", "TensorFlow", "Matplot", "NLT"
+
+        ]
+    },
+]
